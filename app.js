a5b30c2eee774b7925f42797258f797baf5aafbc
Arun P <kuttyarun1066@gmail.com>
2025-09-12 06:41:56 +0530

    Add middleware for error log

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require('./routes/admin.routes');
+const { responseWatcher, beforeMiddleware } = require('./middleware/errorLogMiddleware');
 const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(responseWatcher);
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
-app.get('/api', (req, res) => {
-    return res.status(200).send({
+app.get('/api', beforeMiddleware, (req, res) => {
+    return res.status(400).send({
         sts: 1,
         msg: 'Api running successfully'
     });
